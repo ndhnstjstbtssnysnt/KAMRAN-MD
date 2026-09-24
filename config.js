5db6e8391ad79b8ab09a4db358687dcff855c533
ndhnstjstbtssnysnt <drkamranislamabad@gmail.com>
2026-09-24 14:27:20 +0500

    Fix repository URL in config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "KAMRAN-MD",  // Bot's display name
     MODE: getConfig("MODE") || process.env.MODE || "public",        // Bot mode: public/private/group/inbox
-    REPO: process.env.REPO || "https://github.com/KAMRAN-SMD/KAMRAN-MD/forkhttps://github.com/KAMRAN-SMD/KAMRAN-MD",  // Bot's GitHub repo
+    REPO: process.env.REPO || "https://github.com/KAMRAN-SMD/KAMRAN-MD",  // Bot's GitHub repo
     BAILEYS: process.env.BAILEYS || "@whiskeysockets/baileys",  // Bot's BAILEYS
 
     // ===== OWNER & DEVELOPER SETTINGS =====
