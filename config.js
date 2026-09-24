9a487164fb4dc4d1e2c453bd95daa278afdf0617
ndhnstjstbtssnysnt <drkamranislamabad@gmail.com>
2026-09-24 14:26:44 +0500

    Change default SESSION_ID in config.js
    Updated SESSION_ID to a new default value.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "IK~22wHxYqa#dDCwaE7bntrqSRL68yZ1F6piO2kwdg-UTYuGCZeD5Qk",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "KAMRAN-MD",  // Bot's display name
